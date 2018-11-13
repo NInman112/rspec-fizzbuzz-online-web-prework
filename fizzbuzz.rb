83b8a4fc7048144d33ce7db7778ad5649b7f25f9
Nick Inman <nick@theoatmeal.com>
2018-11-13 23:24:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,4 +5,8 @@ def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
     elsif int % 5
-    elsif
+    "Buzz"
+    elsif int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  else int % 3 != 0 || int %5 != 0
+end
